cb53d2c85cd935b919463a70f09a57cc96e18a7e
Wenega Wama <narcissewatherloy@gmail.com>
2021-10-14 22:55:13 -0300

    first question of the exercise

--- operadores.py
+++ operadores.py
@@ -1,10 +1,38 @@
 import math
 
-numero = 2
-raiz_numero = math.sqrt(numero)
+N = 400
+Res = math.sqrt(N)
+print(Res)
 
-exp2 = math.exp(2)
-print(raiz_numero)
+# # numero = 2
+# # raiz_numero = math.sqrt(numero)
 
-x = dir(math)
-print(raiz_numero, exp2)
+# # exp2 = math.exp(2)
+# # print(raiz_numero)
+
+# # x = dir(math)
+# # print(raiz_numero, exp2)
+
+# a = 3
+# b =  2
+# resposta1 = a / b
+# resposta2 = a // b  # divisao para baixo
+# resposta3 = -a // b
+# resposta4 = b ** (1/2)
+# print(resposta1, resposta2, resposta3,resposta4)   #  resposta3 é muito estranho 
+
+# Exercicio 
+# 1
+a = 3
+b = 5
+c = 2
+
+bhaskara = b ** 2 - 4 * a * c
+x1 = (-b - (bhaskara** 0.5)) / (2 * a)
+x2 = (-b + (bhaskara** 0.5)) / (2 * a)
+
+
+x1_0 = math.ceil(x1)
+x2_0 = math.ceil(x2)
+print('Esses resultados são válidos se o valor do bhaskara é positivo')
+print(f'O valor do bhaskara é : {bhaskara}\nAs raízes reais são {x1_0} e {x2_0} com resultado arredondado para baixo')
