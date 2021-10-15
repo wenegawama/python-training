fe08d249631523c4948279c4a0720353a915b336
Wenega Wama <narcissewatherloy@gmail.com>
2021-10-14 23:58:19 -0300

    Exercicios terminado mas houve problema na 3

--- operadores.py
+++ operadores.py
@@ -1,5 +1,5 @@
 import math
-
+import cmath
 # Exercicio 
 # 1
 a = 3
@@ -23,4 +23,23 @@ x = 8
 raiz_cubica = math.pow(x, 1/3)
 print(raiz_cubica)
 
-#
+#3
+"O resultado é igual pois esta expressão '2*23+7*50' é um string."      
+
+# 4 
+
+numero = 2
+resto = numero % 2
+print('O numero é par se o resto é 0 e impar se o resto é 1')
+n = float(input('Digite um número: '))
+r = n % 2
+if r == 0:
+    print(f'O numero digitado {n} é par')
+else:
+    print(f'O numero digitado: {n} é impar')
+
+
+# 5 Area de um area_do_circulo
+raio = 2
+area_circulo = cmath.pi * math.pow(raio, 2)
+print(f'A area do circulo é : {area_circulo}')
