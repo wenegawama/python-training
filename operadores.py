d3673d1f65e38ed84453b0f3dd4663b21e0103bb
Wenega Wama <narcissewatherloy@gmail.com>
2021-10-18 20:37:30 -0300

    Added works of operadors modules and packs

--- operadores.py
+++ operadores.py
@@ -1,45 +1,89 @@
+# import math
+# import cmath
+# # Exercicio 
+# # 1
+# a = 3
+# b = 5
+# c = 2
+
+# bhaskara = b ** 2 - 4 * a * c
+# x1 = (-b - (bhaskara** 0.5)) / (2 * a)
+# x2 = (-b + (bhaskara** 0.5)) / (2 * a)
+
+
+# x1_0 = math.ceil(x1)
+# x2_0 = math.ceil(x2)
+# print('Esses resultados são válidos se o valor do bhaskara é positivo')
+# print(f'O valor do bhaskara é : {bhaskara}\nAs raízes reais são {x1} e {x2} e {x1_0} e {x2_0} com resultado arredondado para baixo')
+
+
+# #2
+
+# x = 8
+# raiz_cubica = math.pow(x, 1/3)
+# print(raiz_cubica)
+
+# #3
+# "O resultado é igual pois esta expressão '2*23+7*50' é um string."      
+
+# # 4 
+
+# numero = 2
+# resto = numero % 2
+# print('O numero é par se o resto é 0 e impar se o resto é 1')
+# n = float(input('Digite um número: '))
+# r = n % 2
+# if r == 0:
+#     print(f'O numero digitado {n} é par')
+# else:
+#     print(f'O numero digitado: {n} é impar')
+
+
+# # 5 Area de um area_do_circulo
+# raio = 2
+# area_circulo = cmath.pi * math.pow(raio, 2)
+# print(f'A area do circulo é : {area_circulo}')
+
+linha = "*="
+print(30 * linha)
+
+#var_9 = "Olá!"
+#9_var = 75.89
+_var_9 = 5
+print(_var_9)
+
+
+print(30 * linha)
+
+s = 21
+i = 6503
+f = 0.5
+d = 1.7
+
+print( i * f/s,(i / s) + f - 1,s / i,i / f + s * d, i / s   )
+print(30 * linha)
+expressao1 = (23 / 3) / 4 / (19 / 5) - ( (5 - 9) / (7 + 4) ) + (4 + 9)
+expressao2 = (15 / 4) * (3 + 8) / (7 + 4) - 6 + 8
+expressao3 = 27 / 5 / 3 / ( (3 * 2) + 7 + (4 + 4) - 3 )
+expressao4 = 22 / 8 / 3 - 4 + 9 + 4 - 7 / (3 + 5)  
+print(expressao1,expressao2,expressao3,expressao4)
+
+print(30 * linha)
+# Exercicio do fim do capitulo
 import math
 import cmath
-# Exercicio 
-# 1
-a = 3
-b = 5
-c = 2
+valor_digitado = int(input("Digite o valor a calcular : "))
 
-bhaskara = b ** 2 - 4 * a * c
-x1 = (-b - (bhaskara** 0.5)) / (2 * a)
-x2 = (-b + (bhaskara** 0.5)) / (2 * a)
-
-
-x1_0 = math.ceil(x1)
-x2_0 = math.ceil(x2)
-print('Esses resultados são válidos se o valor do bhaskara é positivo')
-print(f'O valor do bhaskara é : {bhaskara}\nAs raízes reais são {x1} e {x2} e {x1_0} e {x2_0} com resultado arredondado para baixo')
-
-
-#2
-
-x = 8
-raiz_cubica = math.pow(x, 1/3)
-print(raiz_cubica)
-
-#3
-"O resultado é igual pois esta expressão '2*23+7*50' é um string."      
-
-# 4 
-
-numero = 2
-resto = numero % 2
-print('O numero é par se o resto é 0 e impar se o resto é 1')
-n = float(input('Digite um número: '))
-r = n % 2
-if r == 0:
-    print(f'O numero digitado {n} é par')
+raiz_quadrada_valor_digitado = math.sqrt(valor_digitado)
+if raiz_quadrada_valor_digitado > 10 :
+    print(True)
 else:
-    print(f'O numero digitado: {n} é impar')
+    print(False)
+    
+raio_circunferencia = raiz_quadrada_valor_digitado
 
+area_circunferencia = cmath.pi * math.pow(raio_circunferencia, 2)
+circunferencia = 2 * cmath.pi * raio_circunferencia
 
-# 5 Area de um area_do_circulo
-raio = 2
-area_circulo = cmath.pi * math.pow(raio, 2)
-print(f'A area do circulo é : {area_circulo}')
+print("Area :",area_circunferencia)
+print("Circunferencia :",circunferencia)
